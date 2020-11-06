6409d92b6d0d9f8b1a5d84e5441fe35d98b01d45
Laurent Maumet <laurent.maumet@gmail.com>
2020-11-06 14:52:06 +0000

    ...

--- .eleventy.js
+++ .eleventy.js
@@ -1,53 +1,87 @@
 const fs = require("fs");
 const path = require("path");
 
-
-const manifestPath = path.resolve(__dirname, "dist", "scripts", "manifest.json");
-const manifest = JSON.parse(fs.readFileSync(manifestPath, {
-  encoding: "utf8"
-}))
+const manifestPath = path.resolve(
+  __dirname,
+  "dist",
+  "scripts",
+  "manifest.json"
+);
+const manifest = JSON.parse(
+  fs.readFileSync(manifestPath, {
+    encoding: "utf8",
+  })
+);
 const pluginSEO = require("eleventy-plugin-seo");
 const embedYouTube = require("eleventy-plugin-youtube-embed");
 
-
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addShortcode("first_image", (post) => extractFirstImage(post));
 
-eleventyConfig.addPlugin(embedYouTube, {
-  embedClass: 'post-video'
-});
+  eleventyConfig.addPlugin(embedYouTube, {
+    embedClass: "post-video",
+  });
+
   eleventyConfig.addPlugin(pluginSEO, {
     title: "AuRorA-5R",
-    description: "Transcubateur. AuRorA-5R accompagne les PME de la région AURA dans leurs projets innovants pour des transitions responsables",
+    description:
+      "Transcubateur. AuRorA-5R accompagne les PME de la région AURA dans leurs projets innovants pour des transitions responsables",
     url: "https://aurora-5r.fr",
     author: "Laurent Maumet",
     twitter: "aurora-5r",
   });
-  eleventyConfig.addPassthroughCopy('src/images')
-  eleventyConfig.addPassthroughCopy('robots.txt')
-  eleventyConfig.addPassthroughCopy('src/posts/images')
-
+  eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("robots.txt");
+  eleventyConfig.addPassthroughCopy("src/posts/images");
 
   // Add a shortcode for bundled CSS.
   eleventyConfig.addShortcode("bundledCss", function () {
-    return manifest["main.css"] ?
-      `<link href="${manifest["main.css"]}" rel="stylesheet" />` :
-      "";
+    return manifest["main.css"]
+      ? `<link href="${manifest["main.css"]}" rel="stylesheet" />`
+      : "";
   });
 
   // Add a shortcode for bundled JS.
   eleventyConfig.addShortcode("bundledJs", function () {
-    return manifest["main.js"] ?
-      `<script src="${manifest["main.js"]}"></script>` :
-      "";
+    return manifest["main.js"]
+      ? `<script src="${manifest["main.js"]}"></script>`
+      : "";
   });
   return {
     dir: {
-      input: 'src',
-      output: 'dist',
-      data: '_data'
+      input: "src",
+      output: "dist",
+      data: "_data",
     },
     passthroughFileCopy: true,
-    templateFormats: ['njk', 'md', 'html', 'yml'],
-    htmlTemplateEngine: 'njk'
+    templateFormats: ["njk", "md", "html", "yml"],
+    htmlTemplateEngine: "njk",
+  };
+};
+
+/**
+ * @param {*} doc A real big object full of all sorts of information about a document.
+ * @returns {String} the markup of the first image.
+ */
+function extractFirstImage(doc) {
+  if (!doc.hasOwnProperty("templateContent")) {
+    console.warn(
+      "❌ Failed to extract image: Document has no property `templateContent`."
+    );
+    return;
   }
+
+  const content = doc.templateContent;
+
+  if (content.includes("<img")) {
+    const imgTagBegin = content.indexOf("<img");
+    const imgTagEnd = content.indexOf(">", imgTagBegin);
+    const res =
+      '<img class="center-block"' +
+      content.substring(imgTagBegin + 4, imgTagEnd + 1);
+    return res;
+    return content.substring(imgTagBegin, imgTagEnd + 1);
+  }
+
+  return "";
 }
--- src/posts/Article_dans_Udimag.md
+++ src/posts/Article_dans_Udimag.md
@@ -1,8 +1,10 @@
 ---
 title: Article dans Udimag
-image: /images/Article_dans_Udimag.md0.png
+author: Laurent
+date: 2020-09-10
+summary: On parle de AuRorA-5R dans Udimag le magazine de l'UDIMEC.
 ---
+
 Dans son numéro 52, le magazine [Udimag](https://www.udimec.fr/sites/default/files/udimag_52_planche_bd.pdf) Consacre un article à AuRorA-5R.
 
 ![image alt text](/images/Article_dans_Udimag.md0.png)
-
