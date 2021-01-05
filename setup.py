d85b8b57058edd896008a502354e8222ebbec2e1
Laurent Maumet <laurent.maumet@gmail.com>
2021-01-05 09:21:33 +0000

    date

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='GsuiteToMarkdown',
+    version='0.1',
+    packages=['gstomd', 'gstomd.test'],
+    author='Laurent Maumet',
+    author_email='laurent@aurora-5r.fr',
+    url='',
+    license='LICENSE',
+    description='',
+    long_description=open('README.rst').read(),
+)
